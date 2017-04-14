97b43d0519837d0c95b09fd2c9a04893b88991c5
Adrien <adrien.crovetto@gmail.com>
2017-04-14 18:38:09 +0200

    code format

--- app/models/User.scala
+++ app/models/User.scala
@@ -6,7 +6,7 @@ import utils.StringUtils
 import scala.concurrent.Future
 
 case class User(
-id:                       String               = StringUtils.generateUuid,
+    id:                       String               = StringUtils.generateUuid,
     created_at:               Option[DateTime]     = Some(DateTime.now(DateTimeZone.UTC)),
     created_by:               Option[String]       = None,
     email:                    String,
@@ -14,10 +14,11 @@ id:                       String               = StringUtils.generateUuid,
     first_name:               Option[String]       = None,
     last_name:                Option[String]       = None,
     deleted_at:               Option[DateTime]     = None
-               )
+)
 
 class UserService {
 
+  // use your own system to manage your user
   def loadById(id:String):Future[Option[User]] = ???
   def findByEmail(email:String):Future[Option[User]] = ???
 
